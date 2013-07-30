074472b48b55fe5b03cdf6a27e7dca8199bdaa53
David Keegan <git@davidkeegan.com>
2013-07-30 15:35:11 -0700

    adding abbreviated day and month to the simplicity watchface

--- src/simplicity.c
+++ src/simplicity.c
@@ -2,7 +2,15 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-#define MY_UUID {0xA4, 0x1B, 0xB0, 0xE2, 0xD2, 0x62, 0x4E, 0xA6, 0xAA, 0x30, 0xED, 0xBE, 0x01, 0xE3, 0x8A, 0x02}
+#define SCREEN_WIDTH 144
+#define SCREEN_HEIGHT 168
+
+#define PADDING 8
+#define LINE_TOP_OFFSET 97
+#define DATE_TOP_OFFSET 68
+#define TIME_TOP_OFFSET 92
+
+#define MY_UUID { 0xEA, 0x7D, 0x3A, 0x11, 0x90, 0xF7, 0x42, 0xD5, 0xBE, 0x6C, 0xF8, 0xA5, 0x51, 0x7C, 0x26, 0x55 }
 PBL_APP_INFO(MY_UUID, "KGN Simple", "@iamkgn", 1, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
 
 Window window;
@@ -12,93 +20,88 @@ TextLayer text_time_layer;
 
 Layer line_layer;
 
+void line_layer_update_callback(Layer *me, GContext* ctx){
+    graphics_context_set_stroke_color(ctx, GColorWhite);
 
-void line_layer_update_callback(Layer *me, GContext* ctx) {
-
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-
-  graphics_draw_line(ctx, GPoint(8, 97), GPoint(131, 97));
-  graphics_draw_line(ctx, GPoint(8, 98), GPoint(131, 98));
+    graphics_draw_line(ctx, GPoint(PADDING, LINE_TOP_OFFSET), GPoint(SCREEN_WIDTH-PADDING, LINE_TOP_OFFSET));
+    graphics_draw_line(ctx, GPoint(PADDING, LINE_TOP_OFFSET+1), GPoint(SCREEN_WIDTH-PADDING, LINE_TOP_OFFSET+1));
 
 }
 
 
-void handle_init(AppContextRef ctx) {
+void handle_init(AppContextRef ctx){
+    window_init(&window, "KGN Simple");
+    window_stack_push(&window, true /* Animated */);
+    window_set_background_color(&window, GColorBlack);
 
-  window_init(&window, "KGN Simple");
-  window_stack_push(&window, true /* Animated */);
-  window_set_background_color(&window, GColorBlack);
-
-  resource_init_current_app(&APP_RESOURCES);
-
-
-  text_layer_init(&text_date_layer, window.layer.frame);
-  text_layer_set_text_color(&text_date_layer, GColorWhite);
-  text_layer_set_background_color(&text_date_layer, GColorClear);
-  layer_set_frame(&text_date_layer.layer, GRect(8, 68, 144-8, 168-68));
-  text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
-  layer_add_child(&window.layer, &text_date_layer.layer);
+    resource_init_current_app(&APP_RESOURCES);
 
 
-  text_layer_init(&text_time_layer, window.layer.frame);
-  text_layer_set_text_color(&text_time_layer, GColorWhite);
-  text_layer_set_background_color(&text_time_layer, GColorClear);
-  layer_set_frame(&text_time_layer.layer, GRect(7, 92, 144-7, 168-92));
-  text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
-  layer_add_child(&window.layer, &text_time_layer.layer);
+    // Date
+    text_layer_init(&text_date_layer, window.layer.frame);
+    text_layer_set_text_color(&text_date_layer, GColorWhite);
+    text_layer_set_background_color(&text_date_layer, GColorClear);
+    layer_set_frame(&text_date_layer.layer, GRect(PADDING, DATE_TOP_OFFSET, SCREEN_WIDTH-PADDING, SCREEN_HEIGHT-DATE_TOP_OFFSET));
+    text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+    layer_add_child(&window.layer, &text_date_layer.layer);
+
+    // Time
+    text_layer_init(&text_time_layer, window.layer.frame);
+    text_layer_set_text_color(&text_time_layer, GColorWhite);
+    text_layer_set_background_color(&text_time_layer, GColorClear);
+    layer_set_frame(&text_time_layer.layer, GRect(PADDING, TIME_TOP_OFFSET, SCREEN_WIDTH-PADDING, SCREEN_HEIGHT-TIME_TOP_OFFSET));
+    text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
+    layer_add_child(&window.layer, &text_time_layer.layer);
+
+    // Line
+    layer_init(&line_layer, window.layer.frame);
+    line_layer.update_proc = &line_layer_update_callback;
+    layer_add_child(&window.layer, &line_layer);
 
 
-  layer_init(&line_layer, window.layer.frame);
-  line_layer.update_proc = &line_layer_update_callback;
-  layer_add_child(&window.layer, &line_layer);
-
-
-  // TODO: Update display here to avoid blank display on launch?
+    // TODO: Update display here to avoid blank display on launch?
 }
 
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
+void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t){
+    // Need to be static because they're used by the system later.
+    static char time_text[] = "00:00";
+    static char date_text[] = "Day, Mon 00";
 
-  // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
-  static char date_text[] = "Xxxxxxxxx 00";
+    char *time_format;
 
-  char *time_format;
-
-
-  // TODO: Only update the date when it's changed.
-  string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
-  text_layer_set_text(&text_date_layer, date_text);
+    // TODO: Only update the date when it's changed.
+    string_format_time(date_text, sizeof(date_text), "%a, %b %e", t->tick_time);
+    text_layer_set_text(&text_date_layer, date_text);
 
 
-  if (clock_is_24h_style()) {
-    time_format = "%R";
-  } else {
-    time_format = "%I:%M";
-  }
+    if(clock_is_24h_style()){
+        time_format = "%R";
+    }else{
+        time_format = "%I:%M";
+    }
 
-  string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+    string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
 
-  // Kludge to handle lack of non-padded hour format string
-  // for twelve hour clock.
-  if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-  }
+    // Kludge to handle lack of non-padded hour format string
+    // for twelve hour clock.
+    if (!clock_is_24h_style() && (time_text[0] == '0')) {
+        memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+    }
 
-  text_layer_set_text(&text_time_layer, time_text);
-
+    text_layer_set_text(&text_time_layer, time_text);
 }
 
 
 void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-
-    .tick_info = {
-      .tick_handler = &handle_minute_tick,
-      .tick_units = MINUTE_UNIT
-    }
-
-  };
-  app_event_loop(params, &handlers);
+    PebbleAppHandlers handlers = {
+        .init_handler = &handle_init,
+        
+        .tick_info = {
+            .tick_handler = &handle_minute_tick,
+            .tick_units = MINUTE_UNIT
+        }
+        
+    };
+    app_event_loop(params, &handlers);
 }
